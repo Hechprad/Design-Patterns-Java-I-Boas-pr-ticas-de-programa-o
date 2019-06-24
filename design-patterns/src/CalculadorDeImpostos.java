2cdbc928357912e5ebfdd22beb21f192950d265d
Jorge Hech <diordiaum@hotmail.com>
2019-06-24 13:30:02 -0300

    teste com o github

--- design-patterns/src/CalculadorDeImpostos.java
+++ design-patterns/src/CalculadorDeImpostos.java
@@ -5,7 +5,7 @@ public class CalculadorDeImpostos {
 			double icms = new ICMS().calculaICMS(orcamento);
 			System.out.println(icms);
 	}
-
+//teste
 	public void realizaCalculoISS(Orcamento orcamento) {
 		double iss = new ISS().calculaISS(orcamento);
 		System.out.println(iss);
