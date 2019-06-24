e14a927f0b936f49ae00216548a8fbc81d42c69a
Hech <diordiaum@hotmail.com>
2019-06-24 13:19:19 -0300

    add files via upload
    arquivos iniciais do curso

---
+++ design-patterns/src/CalculadorDeImpostos.java
@@ -0,0 +1,14 @@
+
+public class CalculadorDeImpostos {
+
+	public void realizaCalculoICMS(Orcamento orcamento, Imposto impostoQualquer) {
+			double icms = new ICMS().calculaICMS(orcamento);
+			System.out.println(icms);
+	}
+
+	public void realizaCalculoISS(Orcamento orcamento) {
+		double iss = new ISS().calculaISS(orcamento);
+		System.out.println(iss);
+	}
+	
+}
