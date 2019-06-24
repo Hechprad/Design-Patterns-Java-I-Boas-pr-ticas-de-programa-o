102a7e29fd25297f5b05321757539378b3ee22b5
Jorge Hech <diordiaum@hotmail.com>
2019-06-24 13:37:02 -0300

    interface de imposto que recebe um orçamento

--- design-patterns/src/CalculadorDeImpostos.java
+++ design-patterns/src/CalculadorDeImpostos.java
@@ -2,13 +2,8 @@
 public class CalculadorDeImpostos {
 
 	public void realizaCalculoICMS(Orcamento orcamento, Imposto impostoQualquer) {
-			double icms = new ICMS().calculaICMS(orcamento);
+			double icms = impostoQualquer.calcula(orcamento);
 			System.out.println(icms);
-	}
-//teste
-	public void realizaCalculoISS(Orcamento orcamento) {
-		double iss = new ISS().calculaISS(orcamento);
-		System.out.println(iss);
 	}
 	
 }
--- design-patterns/src/ISS.java
+++ design-patterns/src/ISS.java
@@ -1,6 +1,8 @@
 
-public class ISS {
-	public double calculaISS(Orcamento orcamento) {
+public class ISS implements Imposto{
+
+	@Override
+	public double calcula(Orcamento orcamento) {
 		return orcamento.getValor() * 0.06;
 	}
 }
