690ffcec1289f69077f9f99af5f71c0af86d64ae
Jorge Hech <diordiaum@hotmail.com>
2019-06-24 15:23:19 -0300

    imposto ICCC criado

---
+++ design-patterns/src/ICCC.java
@@ -0,0 +1,18 @@
+
+public class ICCC implements Imposto {
+
+	@Override
+	public double calcula(Orcamento orcamento) {
+		if(orcamento.getValor() < 1000.0) {
+			return orcamento.getValor() * 0.05;
+		} else if(orcamento.getValor() >= 1000.0 
+				&& orcamento.getValor() <= 3000.0) {
+			return orcamento.getValor() * 0.07;
+		} else if(orcamento.getValor() > 3000.0){
+			return orcamento.getValor() * 0.08 +30.0;
+		} else {
+			return 0;
+		}
+	}
+
+}
