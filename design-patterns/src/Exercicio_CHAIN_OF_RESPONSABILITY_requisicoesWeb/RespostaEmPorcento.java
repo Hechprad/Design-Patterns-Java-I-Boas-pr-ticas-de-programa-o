2b2e0f06690eba716ee87914c2b89cd82a52fef5
Jorge Hech <diordiaum@hotmail.com>
2019-06-27 13:56:33 -0300

    lidando com o final da corrente

--- design-patterns/src/Exercicio_CHAIN_OF_RESPONSABILITY_requisicoesWeb/RespostaEmPorcento.java
+++ design-patterns/src/Exercicio_CHAIN_OF_RESPONSABILITY_requisicoesWeb/RespostaEmPorcento.java
@@ -8,13 +8,17 @@ public class RespostaEmPorcento  implements Resposta {
 	public void responde(Requisicao req, Conta conta) {
 		if (req.getFormato() == Formato.PORCENTO) {
 			System.out.println(conta.getTitular() + "%" + conta.getSaldo());
-		} else {
+		} else if(outraResposta != null) {
 			outraResposta.responde(req, conta);
-		}
+		} else {
+            // não existe próxima na corrente, e ninguém atendeu a requisição!
+            // poderíamos não ter feito nada aqui, caso não fosse necessário!
+            throw new RuntimeException("Formato de resposta não encontrado");
+        }
 	}
 
 	@Override
 	public void setProxima(Resposta resposta) {
-		this.outraResposta = resposta;
+		this.outraResposta = null;
 	}
 }
