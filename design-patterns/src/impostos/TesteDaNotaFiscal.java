b975c20249d61411421d240ec95b7fd08672ba30
Jorge Hech <diordiaum@hotmail.com>
2019-07-01 14:07:58 -0300

    teste mostrando a api que está ficando mais amigável

--- design-patterns/src/impostos/TesteDaNotaFiscal.java
+++ design-patterns/src/impostos/TesteDaNotaFiscal.java
@@ -8,17 +8,12 @@ public class TesteDaNotaFiscal {
 
 	public static void main(String[] args) {
 
-		List<ItemDaNota> itens = Arrays.asList(new ItemDaNota("item 1", 200.0), new ItemDaNota("item 2", 400.0));
-
-		double valorTotal = 0;
-		for (ItemDaNota item : itens) {
-			valorTotal += item.getValor();
-		}
-
-		double impostos = valorTotal * 0.05;
-
-		NotaFiscal nf = new NotaFiscal("razao social", "cnpj", Calendar.getInstance(), valorTotal, impostos, itens,
-				"obs qualquer");
+		CriadorDeNotaFiscal criador = new CriadorDeNotaFiscal();
+		criador.paraEmpresa("Caelum Ensino e Inovação");
+		criador.comCnpj("99.888.777/0001-33");
+		criador.comItem(new ItemDaNota("item 1", 200.0));
+		criador.comItem(new ItemDaNota("item 2", 300.0));
+		criador.comItem(new ItemDaNota("item 3", 400.0));
 
 	}
 
