504858e5fc61437c6db62d66d8b2bda53d36422f
Jorge Hech <diordiaum@hotmail.com>
2019-07-01 11:47:19 -0300

    Teste da variação interna de um objeto (ORÇAMENTO)

--- design-patterns/src/impostos/TesteDeDescontoExtra.java
+++ design-patterns/src/impostos/TesteDeDescontoExtra.java
@@ -3,12 +3,21 @@ package impostos;
 public class TesteDeDescontoExtra {
 
 	public static void main(String[] args) {
-		Orcamento reforma - new Orcamento(500.0);
+		Orcamento reforma = new Orcamento(500.0);
 		
 		reforma.aplicaDescontoExtra();
 		
 		System.out.println(reforma.getValor());
 
+		reforma.aprova();
+		
+		reforma.aplicaDescontoExtra();
+
+		System.out.println(reforma.getValor());
+		
+		reforma.finaliza();
+
+		reforma.aplicaDescontoExtra();
 	}
 
 }
