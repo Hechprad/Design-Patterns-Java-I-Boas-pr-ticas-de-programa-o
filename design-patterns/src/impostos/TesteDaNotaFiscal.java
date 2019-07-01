60704a360ed3ff4b517a8b89c4ac52bf84d38330
Jorge Hech <diordiaum@hotmail.com>
2019-07-01 18:36:00 -0300

    criando um BUILDER para ItemDaNota, apenas para treino

--- design-patterns/src/impostos/TesteDaNotaFiscal.java
+++ design-patterns/src/impostos/TesteDaNotaFiscal.java
@@ -4,9 +4,14 @@ public class TesteDaNotaFiscal {
 
 	public static void main(String[] args) {
 
+		ItemDaNotaBuilder itemBuilder = new ItemDaNotaBuilder();
+		itemBuilder.comNome("ITEM 1").comValor(500.0);
+		ItemDaNota item = itemBuilder.constroi();
+		
 		NotaFiscalBuilder builder = new NotaFiscalBuilder();
 		builder.paraEmpresa("Caelum Ensino e Inovação")
 		.comCnpj("99.888.777/0001-33")
+		.comItem(item)
 		.comItem(new ItemDaNota("item 1", 200.0))
 		.comItem(new ItemDaNota("item 2", 300.0))
 		.comItem(new ItemDaNota("item 3", 400.0))
@@ -15,7 +20,7 @@ public class TesteDaNotaFiscal {
 		
 		NotaFiscal nf = builder.constroi();
 		
-		System.out.println(nf.getRazaoSocial());
+		System.out.println(nf.getValorBruto());
 
 	}
 
